30f55f3641886bb8eda6ec72925850ef6133556b
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-05-24 13:17:15 -0400

    Cleanup

--- nubis/terraform/variables.tf
+++ nubis/terraform/variables.tf
@@ -1,28 +1,53 @@
-variable "account" {
-}
+variable aws_profile {}
 
-variable "region" {
-  default = "us-west-2"
-}
+variable aws_region {}
 
-variable "environment" {
-  default = "stage"
-}
+variable key_name {}
 
-variable "service_name" {
+variable nubis_version {}
+
+variable nubis_domain {}
+
+variable zone_id {}
+
+variable service_name {}
+
+variable environments {}
+
+variable enabled {}
+
+variable technical_contact {}
+
+variable vpc_ids {}
+
+variable subnet_ids {}
+
+variable public_subnet_ids {}
+
+variable ssh_security_groups {}
+
+variable monitoring_security_groups {}
+
+variable internet_access_security_groups {}
+
+variable shared_services_security_groups {}
+
+variable sso_security_groups {}
+
+variable project {
   default = "sso"
 }
 
-variable "instance_type" {
-  default = "t2.medium"
+variable nubis_sudo_groups {
+  default = "nubis_sudo_groups"
 }
 
-variable "ami" {}
-
-variable "client_id" {
-  default=""
+variable nubis_user_groups {
+  default = ""
 }
 
-variable "client_secret" {
-  default=""
+variable "credstash_key" {
+  description = "KMS Key ID used for Credstash (aaaabbbb-cccc-dddd-1111-222233334444)"
 }
+
+variable "credstash_dynamodb_table" {}
