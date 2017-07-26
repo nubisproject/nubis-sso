f230eaa6fa929b88abe5168c8eda9da86d659f5d
limed <limed@mozilla.com>
2017-07-26 15:18:56 -0700

    Add scout url and have scout run once during startup and have a cron for scout to run daily

--- nubis/puppet/scout.pp
+++ nubis/puppet/scout.pp
@@ -1,3 +1,26 @@
 python::pip { 'AWSScout2':
   ensure =>  '3.0.3'
 }
+
+file { '/usr/local/bin/run-scout':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0755',
+  source  =>  'puppet:///nubis/files/run-scout',
+  require => Python::Pip['AWSScout2'],
+}
+
+# This needs to be here so that we can run scout once
+file { '/etc/nubis.d/999-run-scout':
+  ensure  => link,
+  target  => '/usr/local/bin/run-scout',
+  require => File['/usr/local/bin/run-scout'],
+}
+
+file { '/etc/cron.daily/run-scout':
+  ensure  => link,
+  target  => '/usr/local/bin/run-scout',
+  require =>  File['/usr/local/bin/run-scout'],
+}
+
