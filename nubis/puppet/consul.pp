764ce1424bbf6076332a1aca3b45b8580d417259
Philippe M. Chiasson <gozer@ectoplasm.org>
2017-08-08 13:18:43 -0400

    admin service-discovery for Apache-side of things

--- nubis/puppet/consul.pp
+++ nubis/puppet/consul.pp
@@ -19,9 +19,27 @@ file { "${consul_template::config_dir}/top.shtml.ctmpl":
   ],
 }
 
-# Configure our watch
+file { "${consul_template::config_dir}/admin.conf.ctmpl":
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  source  => 'puppet:///nubis/files/admin.conf.ctmpl',
+  require => [
+    Class['consul_template'],
+  ],
+}
+
+# Configure our navigation links
 consul_template::watch { 'top.shtml':
     source      => "${consul_template::config_dir}/top.shtml.ctmpl",
     destination => '/var/www/html/top.shtml',
     command     => '/bin/true',
+}
+
+# Configure Apache Proxy
+consul_template::watch { 'admin.conf':
+    source      => "${consul_template::config_dir}/admin.conf.ctmpl",
+    destination => '/etc/apache2/conf.d/admin.conf',
+    command     => '/etc/init.d/apache2 reload || /etc/init.d/apache2 restart',
 }
