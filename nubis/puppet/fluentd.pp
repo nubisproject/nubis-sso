09a5d4c9315a67abd580d13eee97325ec5cd7d55
Philippe M. Chiasson <gozer@ectoplasm.org>
2018-01-23 15:46:15 -0500

    Upgrade to Traefik v1.5.0  - rework config  - use JSON error logs  - switch to HTTP-01 solving
    Fixes #88

--- nubis/puppet/fluentd.pp
+++ nubis/puppet/fluentd.pp
@@ -1,13 +1,27 @@
 fluentd::configfile { 'traefik': }
 
-fluentd::source { 'traefik':
+fluentd::source { 'traefik-access':
   configfile  => 'traefik',
   type        => 'tail',
   format      => 'json',
   time_format => '%Y-%m-%dT%H:%M:%SZ',
   tag         => 'forward.traefik.access',
   config      => {
-    'path'     => '/var/log/traefik.log',
-    'pos_file' => '/var/log/traefik.log.pos',
+    'read_from_head' => true,
+    'path'           => '/var/log/traefik.log',
+    'pos_file'       => '/var/log/traefik.pos',
+  },
+}
+
+fluentd::source { 'traefik-error':
+  configfile  => 'traefik',
+  type        => 'tail',
+  format      => 'json',
+  time_format => '%Y-%m-%dT%H:%M:%SZ',
+  tag         => 'forward.traefik.error',
+  config      => {
+    'read_from_head' => true,
+    'path'           => '/var/log/traefik_error.log',
+    'pos_file'       => '/var/log/traefik.pos',
   },
 }
